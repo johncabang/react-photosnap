14541ea69008ca0c3afa74788a14790c4a49c5d3
John Cabang <johncabz@gmail.com>
2021-05-21 01:33:49 -0400

    Fix styling - Pricing card component, font-color on hover

--- src/components/PricingSection/PricingSectionElements.js
+++ src/components/PricingSection/PricingSectionElements.js
@@ -15,6 +15,10 @@ export const StyledCardContainer = styled.div`
   margin: 0 165px;
 `;
 
+export const StyledH1 = styled.h1`
+  letter-spacing: 4.17;
+`;
+
 export const StyledButton = styled.button`
   background-color: #000;
   color: #fff;
@@ -25,38 +29,9 @@ export const StyledButton = styled.button`
   height: 40px;
 `;
 
-export const StyledCard = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-  width: 350px;
-  height: 407px;
-  padding: 45px;
-  background-color: #979797;
-  transition: ease 0.3s;
-
-  &:hover {
-    background-color: #000;
-    color: #fff;
-    transform: translate3D(0, -1px, 0) scale(1.11);
-    border-top: 5px solid transparent;
-    border-image: linear-gradient(to right, #ffc593, #bc7198, #5a77ff);
-    border-image-slice: 1;
-    ${StyledButton} {
-      background-color: #fff;
-      color: #000;
-    }
-  }
-`;
-
 export const StyledCardWrapper = styled.div`
   display: flex;
   flex-direction: column;
-`;
-
-export const StyledH1 = styled.h1`
-  letter-spacing: 4.17;
 `;
 
 export const StyledH3 = styled.h3`
@@ -97,5 +72,37 @@ export const StyledSwitch = styled.input`
     transform: translateX(78%);
     background-color: white;
     border: black solid 2px;
+  }
+`;
+
+export const StyledCard = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  width: 350px;
+  height: 407px;
+  padding: 45px;
+  background-color: #979797;
+  transition: ease 0.3s;
+
+  &:hover {
+    background-color: #000;
+    color: #979797;
+    transform: translate3D(0, -1px, 0) scale(1.11);
+    border-top: 5px solid transparent;
+    border-image: linear-gradient(to right, #ffc593, #bc7198, #5a77ff);
+    border-image-slice: 1;
+    ${StyledButton} {
+      background-color: #fff;
+      color: #000;
+    }
+    ${StyledH1} {
+      color: #fff;
+    }
+
+    ${StyledH3} {
+      color: #fff;
+    }
   }
 `;
